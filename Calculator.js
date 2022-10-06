7dfc5fc75a28737dfa009b85917dcafab16a667b
Mateusz Szumlakowski <64979490+Mafiux98@users.noreply.github.com>
2022-10-06 22:05:48 +0200

    Add new test+ readme

--- Calculator.js
+++ Calculator.js
@@ -88,4 +88,5 @@ class Calculator {
     }
   }
 }
+
 module.exports = Calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,13 @@
 const calculator = require("./Calculator");
 const calc = new calculator();
-test("Checking clear operation", () => {
-  calc.clear();
-  expect(this.operation).toBeUndefined();
+describe("Clear funcion testing", () => {
+  test("Checking clear operation", () => {
+    calc.clear();
+    expect(this.operation).toBeUndefined();
+  });
+  test("Checking clear operation", () => {
+    calc.compute((this.operation = "+"));
+    calc.clear();
+    expect(this.operation).toEqual("+");
+  });
 });
