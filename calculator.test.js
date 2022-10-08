66ba4762eba1a7a5584f8556161b52f543c61102
Mateusz Szumlakowski <64979490+Mafiux98@users.noreply.github.com>
2022-10-08 14:29:41 +0200

    I need help with this test

--- calculator.test.js
+++ calculator.test.js
@@ -1,13 +1,27 @@
+const { expect } = require("expect");
 const calculator = require("./Calculator");
-const calc = new calculator();
 describe("Clear funcion testing", () => {
   test("Checking clear operation", () => {
+    const calc = new calculator();
     calc.clear();
     expect(this.operation).toBeUndefined();
   });
   test("Checking clear operation", () => {
+    const calc = new calculator(12, 12);
+    calc.clear();
+    expect(this.currentOperand).toEqual("");
+  });
+  test("Checking clear operation", () => {
+    const calc = new calculator();
     calc.compute((this.operation = "+"));
     calc.clear();
     expect(this.operation).toEqual("+");
+  });
+});
+describe("Delete function testing", () => {
+  test("Deleting last numbers", () => {
+    const calc = new calculator(12, 13);
+    const slice = calc.delete();
+    expect(slice).toEqual(1);
   });
 });
